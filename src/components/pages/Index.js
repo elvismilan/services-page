600a0a081800c2221b5d19ccccc8c7ccb1f128bd
elvismilan <milan.elvis@gmail.com>
2024-04-09 10:37:12 -0400

    fix index.js

---
+++ src/components/pages/Index.js
@@ -0,0 +1,5 @@
+export { Home } from './Home';
+export { Login } from './Login';
+export { Registro } from './Registrarse';
+export { Servicios } from './Servicios';
+export { NotFound } from './NotFound';
