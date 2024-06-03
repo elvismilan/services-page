8d54f27efbf8f2cca06b55291d38fdf22efc41d1
elvismilan <milan.elvis@gmail.com>
2024-06-03 11:28:02 -0400

    add link titulo, imagen open modal addtocart

--- src/components/atoms/Lista.jsx
+++ src/components/atoms/Lista.jsx
@@ -25,11 +25,20 @@ export const Lista = ({servicio,showallicon=false,quantity=0,orden=''}) => {
     <>
       <div className="flex justify-between items-center border-b border-slate-200 py-3 px-2 border-l-4  border-l-transparent mb-5">
         <div className="w-2/3 bg-white flex flex-col space-y-2 p-3  inline-flex items-start space-x-2">
-          <div className="text-slate-500 ">
-            {" "}
-            <strong>{name}</strong>{" "}
+          {
+            showallicon?(
+          <div  className="text-slate-500">
+            {" "}<strong>{name}</strong>{" "}
           </div>
-          <div className="text-slate-500">
+
+            ):(
+          <div onClick={ onAddCart } className="text-slate-500 cursor-pointer">
+            {" "}<strong>{name}</strong>{" "}
+          </div>
+
+            )
+          }
+         <div className="text-slate-500">
             {!!description ? description.substring(0, 85) : ""}
           </div>
           <div className="text-slate-500">
@@ -37,14 +46,32 @@ export const Lista = ({servicio,showallicon=false,quantity=0,orden=''}) => {
           </div>
         </div>
         <div>
-          <img
-            src={imageURL}
-            alt=""
-            width="70"
-            height="70"
-            className="flex-none rounded-md bg-slate-100"
-          />
 
+          {
+            showallicon ? (
+              <>
+                <img
+                  src={imageURL}
+                  alt=""
+                  width="70"
+                  height="70"
+                  className="flex-none rounded-md bg-slate-100"
+                />
+              </>
+            )
+            : (
+              <>
+                <img onClick={ onAddCart }
+                src={imageURL}
+                alt=""
+                width="70"
+                height="70"
+                className="flex-none rounded-md bg-slate-100 cursor-pointer "
+                />
+              </>
+            )
+
+          }
           <div className="mt-2 float-end align-center">
             {showallicon ? (
               <>
