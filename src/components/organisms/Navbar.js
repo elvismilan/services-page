dfe6cc0c72b32c6b08df6efd65b7e6552a188a38
elvismilan <milan.elvis@gmail.com>
2024-06-20 19:01:50 -0400

    buscardor ayudo fix

--- src/components/organisms/Navbar.js
+++ src/components/organisms/Navbar.js
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { ReactSearchAutocomplete } from "react-search-autocomplete";
@@ -8,37 +8,34 @@ import { startLogout } from "../../store";
 import { startListServicios } from "../../store/servicios";
 import {  useNavigate } from "react-router-dom";
 import { googleLogout } from '@react-oauth/google';
+import Input from "../atoms/Input";
 
-  const items = [
-    {
-      id: 0,
-      name: "Cobol",
-    },
-    {
-      id: 1,
-      name: "JavaScript",
-    },
-    {
-      id: 2,
-      name: "Basic",
-    },
-    {
-      id: 3,
-      name: "PHP",
-    },
-    {
-      id: 4,
-      name: "Java",
-    },
-  ];
+import { useForm } from "../../hooks/useForm";
+import { setEmptySearch, updateListService } from "../../store/servicios/serviciosSlice";
+
+  const formData = {
+    search_head: '',
+  }
+
 
 export const Navbar = ({onClick}) => {
-
   const { status } = useSelector( state => state.auth );
   const { services } = useSelector( state => state.servicios );
 
-  //const
+  const { formState, search_head, onInputChange } =useForm(formData);
+
   const dispatch = useDispatch();
+
+   useEffect(() => {
+      if(search_head.length > 2){
+        dispatch( updateListService(search_head) )
+      }else{
+        dispatch( setEmptySearch() )
+      }
+   }, [formState])
+
+
+  //const
   const navigate = useNavigate();
 
   const isCheckingAuthentication =  useMemo( () => status === 'authenticated',[status] );
@@ -65,14 +62,26 @@ export const Navbar = ({onClick}) => {
   <nav className="flex justify-center sm:justify-between flex-col sm:flex-row w-full pt-6">
 
         <div className="flex justify-end flex-row-reverse sm:w-2/3 rounded-2xl  mb-3 sm:mb-0" >
-          <ReactSearchAutocomplete
+
+          <Input
+            type="text"
+            label="Buscar Servicio"
+            name="search_head"
+            value={ search_head }
+            className="w-full rounded-2xl text-secondary search"
+             onChange={ onInputChange }
+            // error={ !!first_nameValid && formSubmitedd }
+            // helperText={ first_nameValid }
+          />
+
+          {/* <ReactSearchAutocomplete
             placeholder="Buscar Servicio"
             items={services}
              onSelect={handleOnSelect}
             styling={{ zIndex: 4 }} // To display it on top of the search box below
             autoFocus
             className="w-full rounded-2xl text-secondary search"
-          />
+          /> */}
         </div>
 
     <div className="text-center sm:text-right md:text-right">
