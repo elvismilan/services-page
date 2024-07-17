a6afd2a082a1657cad255c073ec3b4ffdb50b177
elvismilan <milan.elvis@gmail.com>
2024-07-17 10:50:12 -0400

    redireccionar si no esta logeado

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,29 @@
+import { useSelector } from "react-redux";
 import useModal from "../useModal";
 import Main from "../templates/Main";
 import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
 import Modal from '../molecules/Modal';
 import Service from '../organisms/Inicio';
+import Login from "../organisms/Login";
 
 const Home = () => {
 
+  const estado = useSelector((state) => state.auth.status);
   const {isShowing, toggle} = useModal();
+  const is_logeado = (estado === "authenticated" )
+
   return (
     <Main
       header={<Header/>}
       footer={<Footer />}
     >
       <Modal >
-        <Service />
+        {
+          is_logeado
+          ? ( <Service /> )
+          : ( <Login /> )
+        }
       </Modal>
     </Main>
   )
